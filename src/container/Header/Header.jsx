3d30e407eae20e9c322afcb66b37efee37a702a5
Anna An <arzumanyan.an@gmail.com>
2022-02-15 14:28:18 +0400

    styling the header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,13 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1"> The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque saoien. Et, penatibus aliquam amet tellus.</p>
+      <button type="button" className="custom__button">Explore menu</button>
     </div>
-    <div className="app__wrapper_img"></div>
-  </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
+  </div >
 );
 
 export default Header;
