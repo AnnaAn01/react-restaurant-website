a553510304a478c4333672561fe1fa271fdff4b9
Anna An <arzumanyan.an@gmail.com>
2022-02-16 18:07:15 +0400

    Working on the Special menu section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -9,6 +9,16 @@ const SpecialMenu = () => (
       <SubHeading title="Menu that fits your palatte" />
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, index) => (
+            <p>{wine}</p>
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
