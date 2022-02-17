3aff8c3e8687e218a183083f12a34e5ea0f3fe2e
Anna An <arzumanyan.an@gmail.com>
2022-02-17 15:37:46 +0400

    styling the Special menu section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -14,11 +14,31 @@ const SpecialMenu = () => (
         <p className="app__specialMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine}</p>
+            <p>{wine.title}</p>
           ))}
         </div>
       </div>
+
+      {/*The image in the center */}
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu img" />
+      </div>
+
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <p>{cocktail.title}</p>
+          ))}
+        </div>
+      </div>
+
     </div>
+
+    <div style={{ marginTop: "15px" }}>
+      <button type="button" className="custom__button">View More</button>
+    </div>
+
   </div>
 );
 
