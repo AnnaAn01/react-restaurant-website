2ee9a97456e94ae24c8e3782f5037b27aa49ebe3
Anna An <arzumanyan.an@gmail.com>
2022-02-21 11:48:12 +0400

    styling the Intro section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 import { MdVideoCameraFront } from 'react-icons/md';
 import { meal } from "../../constants";
@@ -6,7 +6,7 @@ import './Intro.css';
 
 const Intro = () => {
   const [playVideo, setsetPlayVideo] = useState(false);
-  const vidRef = React.useRef();
+  const vidRef = useRef();
   const handleVideo = () => {
     setsetPlayVideo((previousPlayVideo) => !previousPlayVideo)
     if (playVideo) {
