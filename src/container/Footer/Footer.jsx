d2cff5ac4783198a32119a2bc73b0fbfc86bdd8f
Anna An <arzumanyan.an@gmail.com>
2022-02-27 11:04:01 +0400

    Creating the Footer section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
 import './Footer.css';
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FaTwitter, FiInstagram } from "react-icons/fi";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
